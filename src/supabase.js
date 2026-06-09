aedeee11481c7ae5e5b30901c7ff25fae95c9c9b
Ripper <ianmaburi@gmail.com>
2026-06-09 23:56:30 +0300

    Move all secrets to environment variables

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://tchbfmlfadrapkaycpdw.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjaGJmbWxmYWRyYXBrYXljcGR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4ODczMjIsImV4cCI6MjA5NDQ2MzMyMn0.83_sxu41Cg-TgXZj7jROoNL0iHIwxIX4BxVkGjfjAyk'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+)
